f02d73020bb3e92476a9fea43e94e235e6490078
floralovecash <79052456+floralovecash@users.noreply.github.com>
2023-04-22 12:59:52 +0800

    Add files via upload

---
+++ src/com/artisan/util/DbUtil.java
@@ -0,0 +1,55 @@
+package com.artisan.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtil {
+	private String dbUrl="jdbc:mysql://localhost:3306/db_student?useUnicode=true&characterEncoding=utf8&autoReconnect=true&useSSL=false"; // ���ݿ����ӵ�ַ
+	private String dbUserName="root"; // �û���
+	private String dbPassword="123456"; // ����
+	private String jdbcName="com.mysql.jdbc.Driver"; // ��������
+	/**
+	 * ��ȡ���ݿ�����
+	 * @return
+	 * @throws Exception
+	 */
+	public Connection getCon(){
+		try {
+			Class.forName(jdbcName);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(dbUrl, dbUserName, dbPassword);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+	/**
+	 * �ر����ݿ�����
+	 * @param con
+	 * @throws Exception
+	 */
+	public void closeCon(Connection con)throws Exception{
+		if(con!=null){
+			con.close();
+		}
+	}
+	
+	public static void main(String[] args) {
+		DbUtil dbUtil=new DbUtil();
+		try {
+			dbUtil.getCon();
+			System.out.println("���ݿ����ӳɹ���");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("���ݿ�����ʧ��");
+		}
+	}
+}
